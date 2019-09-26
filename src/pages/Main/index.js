6ffcfd4b0a042f7a72857fa33ceb8c03fe289236
Alan Nascimento <alan.silva.n@hotmail.com>
2019-09-26 00:02:40 -0300

    Fetching and add users

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
+  const [newUser, setNewUser] = useState('');
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleInputChange = e => setNewUser(e.target.value);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+    };
+
+    setUsers([...users, data]);
+    setNewUser('');
+    setLoading(false);
+    console.log(users);
+  };
+
   return (
     <Container>
       <h1>
         <FaGithubAlt />
-        Repositories
+        Users
       </h1>
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Add repository" />
-        <SubmitButton>
-          <FaPlus color="#fff" size={14} />
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add repository"
+          value={newUser}
+          onChange={handleInputChange}
+        />
+
+        <SubmitButton loading={loading}>
+          {loading ? (
+            <FaSpinner loading={loading} color="#fff" size={14} />
+          ) : (
+            <FaPlus color="#fff" size={14} />
+          )}
         </SubmitButton>
       </Form>
     </Container>
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
   max-width: 700px;
@@ -32,9 +32,20 @@ export const Form = styled.form`
   }
 `;
 
-export const SubmitButton = styled.button.attrs({
+const Rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
   type: 'submit',
-})`
+  disabled: props.loading,
+}))`
   background: #1e2434;
   border: 0;
   padding: 0 15px;
@@ -44,4 +55,17 @@ export const SubmitButton = styled.button.attrs({
   display: flex;
   justify-content: center;
   align-items: center;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+
+  ${props =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${Rotate} 2s linear infinite;
+      }
+    `}
 `;
